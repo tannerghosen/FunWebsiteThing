9aeba94dc7bb76c74fc8f41f85ee52c601d992c7
Tanner Ghosen <tannerghosen@yahoo.com>
2024-06-12 16:14:52 -0400

    Seperated Login and Register from Sandbox

---
+++ Pages/Register.cshtml.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using MySql.Data.MySqlClient;
+using System.Security.Principal;
+
+namespace LearningASPNETAndRazor.Pages
+{
+    public class RegisterModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public RegisterModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
