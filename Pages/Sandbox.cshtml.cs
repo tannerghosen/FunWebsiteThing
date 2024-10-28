4ea38d256a0656f15c3a0343e19d3e9f7d60a682
Tanner Ghosen <tannerghosen@yahoo.com>
2024-10-28 19:39:04 -0400

    Added URL parameter handling to Sandbox to understand it better

--- Pages/Sandbox.cshtml.cs
+++ Pages/Sandbox.cshtml.cs
@@ -4,6 +4,9 @@ namespace FunWebsiteThing.Pages
 {
     public class SandboxModel : PageModel
     {
+        public string? Name { get; private set; }
+        public string? Color { get; private set; }
+
         private readonly ILogger<IndexModel> _logger;
 
         public SandboxModel(ILogger<IndexModel> logger)
@@ -11,9 +14,19 @@ namespace FunWebsiteThing.Pages
             _logger = logger;
         }
 
+        // URL Parameter handling, Model side
         public void OnGet()
         {
+            Name = Request.Query["name"];
+            Color = Request.Query["favcolor"];
 
+            Console.WriteLine("Name: " + Name + " " + "Color: " + Color);
+        }
+        public void OnPost()
+        {
+            Name = Request.Query["name"];
+            Color = Request.Query["favcolor"];
+            Console.WriteLine("Name: " + Name + " " + "Color: " + Color);
         }
     }
 }
