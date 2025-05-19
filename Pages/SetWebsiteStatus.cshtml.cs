a30dbb9f63bfd2b03aee8a0426fa792a70621a84
Tanner Ghosen <tannerghosen@yahoo.com>
2025-05-19 18:22:52 -0400

    Added a new page for a future status bar feature for the website

---
+++ Pages/SetWebsiteStatus.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FunWebsiteThing.Pages
+{
+    public class SetWebsiteStatusModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
