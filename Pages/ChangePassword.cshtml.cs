6ddf44500cd925376a2ac725fd37f85118420ceb
Tanner Ghosen <tannerghosen@yahoo.com>
2025-08-05 22:34:59 -0400

    Changed handling of Id in the ChangePassword model to not change the password of the admin (0) if it's invalid, and to redirect to the same page with an error.

--- Pages/ChangePassword.cshtml.cs
+++ Pages/ChangePassword.cshtml.cs
@@ -29,7 +29,14 @@ namespace FunWebsiteThing.Pages
             Id = Request.Form["Id"];
             if (!string.IsNullOrEmpty(Password))
             {
-                int id = int.TryParse(Id, out int newid) ? newid : 0;
+                int id = int.TryParse(Id, out int newid) ? newid : -2;
+                if (newid == -2)
+                {
+                    Result = "Account Id was not parseable. Contact admins if you're having this issue.";
+                    TempData["Id"] = Id;
+                    return Redirect("/ChangePassword");
+
+                }
                 await SQL.Accounts.UpdateInfo(id, 0, Password, 0, true); 
                 Result = "Password has been changed.";
                 TempData["Id"] = null; // to prevent abuse, we set TempData Id back to null
