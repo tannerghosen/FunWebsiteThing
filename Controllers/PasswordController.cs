4151b62d14a3f55dffab8091dad0166832745537
Tanner Ghosen <tannerghosen@yahoo.com>
2025-12-22 15:07:22 -0500

    Slightly improved PasswordController comment

--- Controllers/PasswordController.cs
+++ Controllers/PasswordController.cs
@@ -9,7 +9,7 @@ namespace FunWebsiteThing.Controllers
         public string Password { get; set; }
     }
     
-    // Routing is pretty simple, api/[controller] is api/Password as the latter part is omitted. So getting Generate would be api/Password/Generate.
+    // Routing is pretty simple, api/[controller] is api/Password as "Controller" is omitted. So getting Generate would be api/Password/Generate.
     // If you wanted it to simply be /(Action), you'd blank the Route string. Simply api/(Action), make Route "api/", etc.
     [ApiController]
     [Route("api/[controller]")]
