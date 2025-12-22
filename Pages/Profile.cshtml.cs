da6402442c832ca1383864ee315cc4eea658ed9f
Tanner Ghosen <tannerghosen@yahoo.com>
2025-12-22 14:17:10 -0500

    Added an account type addition to the Profile page that tells visitors if the account is a member, admin, or super admin (website owner).

--- Pages/Profile.cshtml.cs
+++ Pages/Profile.cshtml.cs
@@ -17,10 +17,13 @@ namespace FunWebsiteThing.Pages
         [BindProperty]
         public int? TotalComments { get; set; }
 
+        [BindProperty]
+        public string? AccountType { get; set; }
         public void OnGet()
         {
             Id = Convert.ToInt32(Request.Query["UserId"]);
             Username = SQL.Accounts.GetUsername(Id) == null || SQL.Accounts.GetUsername(Id) == string.Empty ? "Not Registered" : SQL.Accounts.GetUsername(Id);
+            AccountType = SQL.Admin.IsAdmin(Id) == true ? Id == 1 ? "Super Admin" : "Admin" : "Member";
             JoinDate = SQL.Accounts.GetJoinDate(Id) == null || SQL.Accounts.GetJoinDate(Id) < DateTime.MinValue ? DateTime.Now : SQL.Accounts.GetJoinDate(Id);
             TotalComments = SQL.Comments.CountCommentsByUserId(Id);
         }
