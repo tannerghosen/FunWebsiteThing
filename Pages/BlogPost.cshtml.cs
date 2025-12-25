0667df23762a8e1ba137f48d1d7fed59c13764e3
Tanner Ghosen <tannerghosen@yahoo.com>
2025-12-25 18:50:50 -0500

    Changed the check from IsNullOrWhitepsace to IsNullOrEmpty for Title and Message in BlogPost.cshtml.cs

--- Pages/BlogPost.cshtml.cs
+++ Pages/BlogPost.cshtml.cs
@@ -26,7 +26,7 @@ namespace FunWebsiteThing.Pages
             int blogid = SQL.Blog.GetBlogPostCount() + 1;
             if (HttpContext.Session.GetInt32("IsAdmin") == 1 && SQL.Admin.IsAdmin(HttpContext.Session.GetInt32("UserId")))
             {
-                if(!string.IsNullOrWhiteSpace(Title) && !string.IsNullOrWhiteSpace(Message))
+                if(!string.IsNullOrEmpty(Title) && !string.IsNullOrEmpty(Message))
                 {
                     await SQL.Blog.AddBlogPost(Title, Message);
                 }
